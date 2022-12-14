2f4d929376cbcec9e0a672720341d1db75a4fe47
7tsuno <natsumeg.music@gmail.com>
2022-12-14 22:39:41 +0900

    vercel analyticsを入れてみた

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>;
 }
